58ab307d8858a23938bd060c00c952d97f1fb767
julietteabeyta <juneauwh0a@gmail.com>
2018-07-05 11:02:45 -0600

    Add build script to build file and add build dir to the gitignore

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,9 +10,26 @@ gulp.task("default", () =>{
 gulp.task("sass", () => {
 	gulp.src(scssPaths)
 		.pipe(sass())
-		.pipe(gulp.dest("./css"));
-})
+		.pipe(gulp.dest("./build/css"));
+});
+
+gulp.task("html", function () {
+	return gulp.src('./index.html')
+		.pipe(gulp.dest("./build/"))
+});
+
+gulp.task("images", function () {
+	return gulp.src(["./images/*"])
+		.pipe(gulp.dest("./build/images/"));
+});
+
+gulp.task("js", function () {
+	return gulp.src(["./js/*"])
+		.pipe(gulp.dest("./build/js/"));
+});
 
 gulp.task("watch", () => {
 	gulp.watch(scssPaths, ["sass"]);
-})
+});
+
+gulp.task("build", ["sass", "js", "html", "images"]);
