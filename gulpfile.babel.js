70a4a288741dc2af4794a9650e3b135abf930e0f
julietteabeyta <juneauwh0a@gmail.com>
2018-07-05 12:12:11 -0600

    Add in fonts and add font handling to gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,9 +9,20 @@ gulp.task("sass", () => {
 		.pipe(gulp.dest("./build/css"));
 });
 
+gulp.task("localsass", () => {
+	gulp.src(scssPaths)
+		.pipe(sass())
+		.pipe(gulp.dest("./css"));
+});
+
 gulp.task("html", function () {
 	return gulp.src('./index.html')
 		.pipe(gulp.dest("./build/"))
+});
+
+gulp.task("fonts", function () {
+	return gulp.src('./fonts/Salome/*')
+		.pipe(gulp.dest("./build/fonts/Salome/"))
 });
 
 gulp.task("images", function () {
@@ -28,4 +39,6 @@ gulp.task("watch", () => {
 	gulp.watch(scssPaths, ["sass"]);
 });
 
-gulp.task("build", ["sass", "js", "html", "images"]);
+gulp.task("build", ["sass", "fonts", "localsass", "js", "html", "images"]);
+
+gulp.task("default", ["build"]);
